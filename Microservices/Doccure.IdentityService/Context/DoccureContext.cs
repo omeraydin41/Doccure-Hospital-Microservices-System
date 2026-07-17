9180234ae21bb68a7208ec14fcac3fbbbf8d1d9d
omeraydin41 <omerenver1317@gmail.com>
2026-07-17 16:04:18 +0300

    ENG : The Identity service was added to allow users to register with the system and create their own accounts (profiles) as members or for membership purposes. The packages were installed, entities were added, and a database connection was set up via the context class. Changes were made to Program.cs.
    TUR : Üye veya Üyelik durumunda kullanıcının sisteme kayıt olarak kendine ait bir hesap (profil) oluşturması durumu için Identity servisi eklendi.
    Paketler kuruldu ve Entitiyler eklenip context classı uzerinden veri tabanı bağlantısı hazırlandı. Program.cs kayıtlar yapıldı.

---
+++ Microservices/Doccure.IdentityService/Context/DoccureContext.cs
@@ -0,0 +1,26 @@
+﻿using Doccure.IdentityService.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Doccure.IdentityService.Context
+{
+    public class DoccureContext : IdentityDbContext<AppUser>//<>  tıp guvenliği için IdentityDbContext kullanıyoruz. AppUser sınıfımızı burada belirtiyoruz.
+    {
+        public DoccureContext(DbContextOptions<DoccureContext> options) : base(options)
+        {
+        }
+
+      
+
+        /*
+          IdentityDbContext İLE OLUŞAN TABLOLAR
+        AspNetUsers      (Kullanıcı bilgilerinin tutulduğu tablo)
+        AspNetRoles      (Rollerin tutulduğu tablo)
+        AspNetUserRoles  (Kullanıcılar ve roller arasındaki ilişki tablosu)
+        AspNetUserClaims (Kullanıcı yetki/tanım etiketleri)
+        AspNetRoleClaims (Rol yetki/tanım etiketleri)
+        AspNetUserLogins (Google, Facebook gibi dış giriş bilgileri)
+        AspNetUserTokens (Şifre sıfırlama, 2FA gibi geçici tokenlar)
+         */
+    }
+}
---
+++ Microservices/Doccure.IdentityService/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Doccure.IdentityService.Entities
+{
+    public class AppUser:IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Gender { get; set; }
+        public string? BloodGroup { get; set; }
+    }
+}
